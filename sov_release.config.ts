674a7b33ee6f0f4668d9355693c3140c7de49f74
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-06 14:06:41 +0100

    Release: 3.7.7

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -5,7 +5,7 @@ const releaseConfig: ReleaseConfig = {
     {
       directory: "./",
       updateDeps: true,
-      version: "3.7.6",
+      version: "3.7.7",
       release: true,
       test: false,
       lintAndBuild: true,
