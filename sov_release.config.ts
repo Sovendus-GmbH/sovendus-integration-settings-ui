218a91ffef83e94d0c4fb73c0740a5d9af5ae4d1
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-04-23 13:45:41 +0200

    Release: 4.3.15

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -9,7 +9,7 @@ const releaseConfig: ReleaseConfig = {
       lint: true,
       build: true,
       release: {
-        version: "4.3.14",
+        version: "4.3.15",
         versionBumper: [
           {
             filePath: "src/package/utils/constants.ts",
--- src/package/utils/constants.ts
+++ src/package/utils/constants.ts
@@ -1,1 +1,1 @@
-export const version = "4.3.14";
+export const version = "4.3.15";
